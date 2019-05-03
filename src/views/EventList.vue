7663a445e3130960343a66dd1ab1d4056c50dc14
Ferniwolf <siul9713@hotmail.com>
2019-05-02 18:49:42 -0600

    repositorio
    team2

---
+++ src/views/EventList.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>Event Listing</h1>
+        <EventCard v-for="event in events" :key="event.id" :event="event"/> 
+        <BaseIcon/>
+    </div>
+</template>
+
+<script>
+import EventCard from '@/components/EventCard.vue';
+import axios from 'axios'
+export default {
+    components:{
+        EventCard
+    },
+
+created() {
+    axios.get('http://localhost:3000/events').then(response => {
+        console.log(response.data)
+        this.events = response.data
+    })
+    .catch(error =>{
+        console.log('No jala carnal: ' + error.response)
+    })
+}
+};  
+</script>
